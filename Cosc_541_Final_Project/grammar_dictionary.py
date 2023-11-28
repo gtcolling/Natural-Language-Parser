15766cd99e088214c6fe47eaf67d5d9cf440a310
acampb46 <acampb46@emich.edu>
2023-11-27 23:47:44 -0500

    Added additional logic to the grammar check function to ensure user input follows our predefined grammar. Also included powerpoint Presentation for ease of access.

--- Cosc_541_Final_Project/grammar_dictionary.py
+++ Cosc_541_Final_Project/grammar_dictionary.py
@@ -5,34 +5,104 @@ grammar = {"sub": ['I', 'We'],
            "prep": ['at', 'in', 'with', 'to', 'below', 'along', 'above', 'on', 'over', 'off'],
            "adj": ['cold', 'sunny', 'mean', 'cool', 'rough', 'damp', 'smooth', 'warm', 'rainy', 'hot'],
            "art": ['the', 'a', 'an'],
-           "noun": ['office', 'banana', 'dog', 'cat', "umbrella", "hat", "stone", "park", "car", "breakfast"]
+           "noun": ['office', 'banana', 'dog', 'cat', 'umbrella', 'hat', 'stone', 'park', 'car', 'breakfast']
            }
 
 
 def find_in_dict(tokens, dictionary):
     i = 0
     for string in tokens:
+        valid = False
         for key, value in grammar.items():
             if string in value:
                 if key in dictionary.keys():
-                    dictionary[key].append({string: i})
+                    dictionary[key].append((string, i))
+                    valid = True
                 else:
                     dictionary[key] = []
-                    dictionary[key].append({string: i})
+                    dictionary[key].append((string, i))
+                    valid = True
+        if not valid:
+            dictionary["not_found"] = string
         i += 1
+
     return dictionary
 
 
 def check_grammar(dictionary):
-    subject = False
-    verb = False
-    for key in dictionary.keys():
-        if key == "sub":
-            subject = True
-        elif key == "verb":
-            verb = True
+    subject_bool = False
+    verb_bool = False
+    sub = list()
+    advp = list()
+    verb = list()
+    prep = list()
+    np = list()
+    adjp = list()
+    art = list()
 
-    if subject and verb:
+    for key, value in dictionary.items():
+
+        # If "I" or "We" is the beginning of the sentence, set subject_bool to true.
+        rng = range(len(dictionary[key]))
+        if key == "sub":
+            if dictionary[key][0][1] == 0:
+                sub.append(dictionary[key][0][0])
+                subject_bool = True
+            else:
+                print("Subject must come first in your sentence.")
+
+        # Create an adverb phrase (broken by verbs, nouns, articles, adjectives).
+        elif key == "adv":
+            for i in rng:
+                advp.append(dictionary[key][i][0])
+                if (i + 1 >= len(dictionary[key])) or ((dictionary[key][i][1]) != int((dictionary[key][i + 1][1]) - 1)):
+                    break
+
+        # If verb comes after the subject, set verb_bool to true.
+        elif key == "verb":
+            if dictionary[key][0][1] > 0:
+                verb.append(dictionary[key][0][0])
+                verb_bool = True
+            else:
+                print("Verb must come after the subject of your sentence.")
+
+        # Load the prep list with the preposition
+        elif key == "prep":
+            prep.append(dictionary[key][0][0])
+
+        # Load the art list with an article
+        elif key == "art":
+            art.append(dictionary[key][0][0])
+
+        # Create an adjective phrase (as many adjectives in a row as provided by the user).
+        elif key == "adj":
+            for j in rng:
+                adjp.append(dictionary[key][j][0])
+                if (j + 1 >= len(dictionary[key])) or ((dictionary[key][j][1]) != int((dictionary[key][j + 1][1]) - 1)):
+                    break
+
+        # Create a noun phrase using the article, adjective phrase, and noun
+        elif key == "noun":
+            np.append(' '.join(str(e) for e in art))
+            np.append(' '.join(str(e) for e in adjp))
+            np.append(str(dictionary[key][0][0]))
+
+        # If there is a word in the input that isn't in the parser, the sentence is invalid.
+        elif key == "not_found":
+            print("Your input contains a word not accepted by this parser. Please try again.")
+            return False
+
+    # Since subject and verb are the only non-nullable aspects of the grammar,
+    # they must be present for a passing sentence.
+    if subject_bool and verb_bool:
+        print("Your sentence has been broken down as follows: " +
+              "Subject: " + ' '.join(str(e) for e in sub) + ", " +
+              ("Adverb Phrase: " + ' '.join(str(e) for e in advp) + ", " if len(advp) > 0 else "") +
+              "Verb: " + ' '.join(str(e) for e in verb) + ", " +
+              ("Preposition: " + ' '.join(str(e) for e in prep) + ", " if len(prep) > 0 else "") +
+              ("Noun Phrase: " + ' '.join(str(e) for e in np) + "\n" if len(np) > 0 else "\n"))
         return True
+
+    # If subject and verb are not present, the sentence automatically fails.
     else:
         return False
--- Cosc_541_Final_Project/main.py
+++ Cosc_541_Final_Project/main.py
@@ -2,25 +2,25 @@ from grammar_dictionary import find_in_dict, check_grammar
 
 
 def main():
-    # accept user input
+    # Accept user input
     sentence = input("Please enter a sentence: ")
-    print("You entered: " + sentence)
 
-    # tokenize string
+    # Tokenize string
     tokens = sentence.split()
 
-    # initialize empty dictionary to hold key:value pairs
+    # Initialize empty dictionary to hold key:value pairs
     tree = {}
 
-    # find if tokens exist in predefined lists for the grammar, keeping location of
-    # each word in the string to form phrases
+    # Find if tokens exist in predefined lists for the grammar, keeping location of
+    # Each word in the string to form phrases
     tree.update(find_in_dict(tokens, tree))
 
-    # check and see if the sentence violates our grammar
+    # Check and see if the sentence violates our grammar
     if not check_grammar(tree):
-        print("Your input is not a valid sentence.")
+        print("Your input is not valid. Please try again.")
     else:
         print("Your input passes!")
+
 
 if __name__ == '__main__':
     main()
